dc9e36cc12ff7205c096788fa13f5adf6744f661
D7703_02 <d7703_02@D7703-02ui-iMac.local>
2018-03-28 17:08:41 +0900

    and

--- PrimeNumberChaek1/ViewController.swift
+++ PrimeNumberChaek1/ViewController.swift
@@ -1,25 +1,48 @@
-//
-//  ViewController.swift
-//  PrimeNumberChaek1
-//
-//  Created by D7703_02 on 2018. 3. 28..
-//  Copyright © 2018년 D7703_02. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate{
 
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTextField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func btReset(_ sender: UIButton) {
+        myTextField.text = ""
+        resultLabel.text = ""
     }
-
+    
+    @IBAction func btPrime(_ sender: UIButton) {
+            let number = Int(myTextField.text!)
+            var isPrime = true
+            if number! == 1{
+                isPrime = false
+            }
+            if number! != 1 && number! != 2 {
+                for i in 2 ..< number! {
+                    if number! % i == 0{
+                        isPrime = false
+                    }
+                }
+            }
+            if isPrime{
+                resultLabel.text = "Prime Number"
+            } else{
+                resultLabel.text = "Not Prime Number"
+        
+            }
+    
+       
 
 }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        myTextField.resignFirstResponder()
+        return true
+    }
+}
+
 
